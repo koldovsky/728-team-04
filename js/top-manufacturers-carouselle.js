d89025a165f6135e9c0e71e85ba16a4bd96c02b6
TxYtf <119027598+TxYtf@users.noreply.github.com>
2023-01-10 12:37:17 +0000

    Top manufacturers carouselle

--- js/top-manufacturers-carouselle.js
+++ js/top-manufacturers-carouselle.js
@@ -1,10 +1,10 @@
 (function(){
 
     const slides = [
-        '<div class="top-manufacturers-slide"><img src="./img/ford-logo.png" alt="Ford trademark"></div>',
-        '<div class="top-manufacturers-slide"><img src="./img/honda-logo.png" alt="Honda trademark"></div>',
-        '<div class="top-manufacturers-slide"><img src="./img/bmw-logo.png" alt="BMW trademark"></div>',
-        '<div class="top-manufacturers-slide"><img src="./img/audi-logo.png" alt="Audi trademark"></div>'
+        '<div class="top-manufacturers-slide"><img src="img/ford-logo.png" alt="Ford trademark" width="200"></div>',
+        '<div class="top-manufacturers-slide"><img src="img/honda-logo.png" alt="Honda trademark" width="200"></div>',
+        '<div class="top-manufacturers-slide"><img src="img/bmw-logo.png" alt="BMW trademark" width="200"></div>',
+        '<div class="top-manufacturers-slide"><img src="img/audi-logo.png" alt="Audi trademark" width="200"></div>'
     ];
     
     let currentSlideIdx = 0;
@@ -18,4 +18,8 @@
         currentSlideIdx = (currentSlideIdx + 1) >= slides.length ? 0 : currentSlideIdx + 1;
         renderSlide();
     };
+
+    setInterval (nextSlide, 3000);
+
+    renderSlide ();
 })()
