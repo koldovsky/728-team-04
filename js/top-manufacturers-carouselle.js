ce01cc9621e2f48a9caac6a5fe418da5a65b1171
TxYtf <119027598+TxYtf@users.noreply.github.com>
2023-01-10 12:04:35 +0000

    Top manufacturers carouselle

--- js/top-manufacturers-carouselle.js
+++ js/top-manufacturers-carouselle.js
@@ -5,5 +5,11 @@
         '<div class="top-manufacturers-slide"><img src="./img/honda-logo.png" alt="Honda trademark"></div>',
         '<div class="top-manufacturers-slide"><img src="./img/bmw-logo.png" alt="BMW trademark"></div>',
         '<div class="top-manufacturers-slide"><img src="./img/audi-logo.png" alt="Audi trademark"></div>'
-    ]
+    ];
+    
+    let currentSlideIdx = 0;
+    
+    function renderSlide {
+
+    };
 })()
